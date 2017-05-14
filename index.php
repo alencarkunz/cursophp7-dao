b74f48e9cb6e3baf5eb649f89a6ff37bac555852
alencarkunz <alenkar.k@gmail.com>
2017-05-14 11:34:59 -0300

    lista de usuários

--- index.php
+++ index.php
@@ -6,10 +6,17 @@ require_once('config.php');
 //$usuarios = $sql->select('SELECT * FROM tb_usuarios');
 //echo json_encode($usuarios);
 
+//$usuario = new Usuario();
+//$usuario->loadById(2);
+//echo $usuario;
+
+//echo json_encode(Usuario::getList());
+
+//$search = Usuario::search('u');
+//echo json_encode($search);
+
 $usuario = new Usuario();
-
-$usuario->loadById(2);
-
+$usuario->login('user','12345');
 echo $usuario;
 
 ?>
