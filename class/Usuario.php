6c8a1f1bfcb044fa3cead6a38571f9355151a91b
alencarkunz <alenkar.k@gmail.com>
2017-05-14 12:13:57 -0300

    delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -113,6 +113,19 @@ class Usuario {
 		$sql->query('UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID',array(':LOGIN'=>$this->getDeslogin(),':PASSWORD'=>$this->getDessenha(),':ID'=>$this->getIdusuario()));
 	}
 
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query('DELETE FROM tb_usuarios WHERE idusuario = :ID', array(':ID'=>$this->getIdusuario()));
+
+		$this->setIdusuario('');
+		$this->setDeslogin('');
+		$this->setDessenha('');
+		$this->setDtcadastroa(new DateTime());
+
+	} 
+
 	public function __construct($login = '', $password = '') {
 
 		$this->setDeslogin($login);
--- index.php
+++ index.php
@@ -2,31 +2,41 @@
 
 require_once('config.php');
 
+//banco de dados
 //$sql = new Sql();
 //$usuarios = $sql->select('SELECT * FROM tb_usuarios');
 //echo json_encode($usuarios);
 
+// buscar
 //$usuario = new Usuario();
 //$usuario->loadById(2);
 //echo $usuario;
 
 //echo json_encode(Usuario::getList());
 
+// buscar
 //$search = Usuario::search('u');
 //echo json_encode($search);
 
+//login
 //$usuario = new Usuario();
 //$usuario->login('user','12345');
 //echo $usuario;
 
+// insert
 //$aluno = new Usuario('aluno','@luno');
 //$aluno->insert();
 //echo $aluno;
 
+// alterar
+//$usuario = new Usuario();
+//$usuario->loadById(7);
+//$usuario->update('professor','senhaprof');
+//echo $usuario;
 
 $usuario = new Usuario();
 $usuario->loadById(7);
-$usuario->update('professor','senhaprof');
+$usuario->delete();
 echo $usuario;
 
 ?>
